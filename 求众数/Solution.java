24c90eb29f870f3b9b86cf59ceee444272844627
倪力烽 <nilifeng2015@outlook.com>
2018-09-19 23:33:36 +0800

    leetcode 169. 求众数
    上次文件传错，现改正

--- 求众数/Solution.java
+++ 求众数/Solution.java
@@ -1,32 +1,36 @@
 /**
- * leetcode 766. 托普利茨矩阵
- * 如果一个矩阵的每一方向由左上到右下的对角线上具有相同元素，那么这个矩阵是托普利茨矩阵。
- * 给定一个 M x N 的矩阵，当且仅当它是托普利茨矩阵时返回 True。
+ * leetcode 169. 求众数
+ * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
  *
- * 输入:
- *    输入: 
- *   matrix = [
- * [1,2,3,4],
- * [5,1,2,3],
- * [9,5,1,2]
- * ]
- * 输出: True
- * 解释:
- * 在上述矩阵中, 其对角线为:
- * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]"。
- *  各条对角线上的所有元素均相同, 因此答案是True。
+ * 示例 1:
+ * 输入: [3,2,3]  输出: 3
+ *
+ * 示例 2:
+ * 输入: [2,2,1,1,1,2,2] 输出: 2
  */
 class Solution {
-    public boolean isToeplitzMatrix(int[][] matrix) {
-        int a = matrix.length;
-        int b = matrix[0].length;
-        for(int i=0;i<a-1;i++){
-            for(int j=0;j<b-1;j++){
-                if(matrix[i][j]!=matrix[i+1][j+1]){
-                    return false;
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int s = 0;
+        if(n % 2 == 0) {
+            //偶数
+            for(int i = 0; i< n/2;i++) {
+                if(nums[i] == nums[i + n/2]) {
+                    s = nums[i]; // 如果
+                    break;
+                }
+            }
+        }else {
+            // 奇数
+            for(int i = 0; i< n/2 + 1;i++) {
+                if(nums[i] == nums[i + n/2]) {
+                    s = nums[i]; // 如果
+                    break;
                 }
             }
         }
-        return true;
+        return s;
     }
 }
